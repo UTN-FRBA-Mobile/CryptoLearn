94617b9f4ff68ad045bd62ded9022b75568922c3
Nicolas Coen <nicocoen@frba.utn.edu.ar>
2022-05-16 21:17:53 -0300

    feat: added bindings and navigation from login to signup/home

--- app/src/main/java/com/mobile/test/LoginFragment.kt
+++ app/src/main/java/com/mobile/test/LoginFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.mobile.test.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,8 @@ class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +41,33 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding?.loginButton?.setOnClickListener{
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            when{
+                email.isEmpty() or password.isEmpty() -> {
+                    // TODO: Show toast
+                    print("Llena los campos mi rey")
+                }
+                else ->{
+                    val bundle = bundleOf("Email" to email,"Password" to password)
+                    val action = R.id.action_loginFragment_to_homeFragment
+                    findNavController().navigate(action, bundle)
+                }
+            }
+        }
+
+        _binding?.signUp?.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
     }
 
     companion object {
