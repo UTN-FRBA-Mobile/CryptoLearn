ecae630ab11d23bf697969d4a7ed81052ef1fa48
Nicolas Coen <nicocoen@frba.utn.edu.ar>
2022-05-23 00:35:41 -0300

    feat: added navigation from home buttons to chapters

--- mobile-app/app/src/main/java/com/mobile/test/ChaptersAdapter.kt
+++ mobile-app/app/src/main/java/com/mobile/test/ChaptersAdapter.kt
@@ -1,18 +1,15 @@
 package com.mobile.test
 
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.Drawable.createFromPath
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.TextView
-import androidx.annotation.DrawableRes
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.mobile.test.model.Chapter
-import com.mobile.test.model.Level
 
-class ChaptersAdapter(private val chapters: MutableList<Chapter>):
+class ChaptersAdapter(private val chapters: MutableList<Chapter>) :
     RecyclerView.Adapter<ChaptersAdapter.MyViewHolder>() {
 
     class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
@@ -31,6 +28,10 @@ class ChaptersAdapter(private val chapters: MutableList<Chapter>):
 //        val image = chapters[position].image
 //        holder.view.findViewById<TextView>(R.id.chapter_image).background = createFromPath("res/drawable/chapter_3.png")
         holder.view.findViewById<TextView>(R.id.chapter_name).text = chapters[position].name
+        // TODO: Send specific information to each chapter with navigation
+        holder.view.findViewById<Button>(R.id.chapter_image).setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_chapterFragment)
+        }
     }
 
 /*    override fun getItemViewType(position: Int): Int {
