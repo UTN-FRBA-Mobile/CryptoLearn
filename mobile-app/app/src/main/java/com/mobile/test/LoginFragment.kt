b2b0a2276452341373f780533105af9e112d7cfa
Ezequiel Coen <ecoen@kovix-group.com>
2022-07-05 02:02:03 -0300

    feat: enable biometric login

--- mobile-app/app/src/main/java/com/mobile/test/LoginFragment.kt
+++ mobile-app/app/src/main/java/com/mobile/test/LoginFragment.kt
@@ -1,20 +1,22 @@
 package com.mobile.test
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.bundleOf
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.mobile.test.api.RetrofitClient
+import com.mobile.test.api.*
+import com.mobile.test.biometrics.*
 import com.mobile.test.databinding.FragmentLoginBinding
-import com.mobile.test.api.LoginRequest
-import com.mobile.test.api.LoginResponse
-import com.mobile.test.api.SessionManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,8 +24,6 @@ import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -32,17 +32,39 @@ private const val ARG_PARAM2 = "param2"
  */
 class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val TAG = "EnableBiometricLogin"
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
-    private lateinit var sessionManager: SessionManager
+    private lateinit var biometricPrompt: BiometricPrompt
+    private val cryptographyManager = CryptographyManager()
+    private val ciphertextWrapper
+        get() = cryptographyManager.getCiphertextWrapperFromSharedPrefs(
+            requireContext(),
+            BIOMETRICS_SHARED_PREFS_FILENAME,
+            Context.MODE_PRIVATE,
+            CIPHERTEXT_WRAPPER_KEY
+        )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        val canAuthenticate = BiometricManager.from(requireContext()).canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS) {
+            if (ciphertextWrapper != null) {
+                showBiometricPromptForDecryption()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val canAuthenticate = BiometricManager.from(requireContext()).canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS) {
+            if (ciphertextWrapper != null) {
+                showBiometricPromptForDecryption()
+            }
         }
     }
 
@@ -71,14 +93,12 @@ class LoginFragment : Fragment() {
                     toast.show()
                 }
                 else ->{
-                    sessionManager = SessionManager.getInstance(requireContext())
                     RetrofitClient.service.login(LoginRequest(email, password))
                         .enqueue(object: Callback<LoginResponse> {
                             override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                                 if (response.isSuccessful) {
-                                    sessionManager.saveAuthToken(response.body()?.token!!)
-                                    val action = R.id.action_loginFragment_to_homeFragment
-                                    findNavController().navigate(action)
+                                    SessionManager.getInstance(requireContext()).saveAuthToken(response.body()?.token!!)
+                                    showActivateBiometricsDialog()
                                 } else {
                                     val toast = Toast.makeText(context, resources.getString(R.string.bad_login), Toast.LENGTH_LONG)
                                     toast.setGravity(Gravity.CENTER_VERTICAL, 0, -150);
@@ -117,23 +137,83 @@ class LoginFragment : Fragment() {
             binding.email.text.toString().isNotEmpty() && binding.password.text.toString().isNotEmpty()
     }
 
+    private fun showActivateBiometricsDialog(){
+        AlertDialog.Builder(requireContext())
+            .setTitle("Iniciar sesion con huella")
+            .setMessage("¿Quiere activar el inicio de sesion con huella?")
+            .setPositiveButton("Si!") { _, _ ->
+                showBiometricPromptForEncryption()
+            }
+            .setNegativeButton("No, gracias"){ _, _ ->
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            }
+            .show()
+    }
+
+    private fun showBiometricPromptForEncryption() {
+        val canAuthenticate = BiometricManager.from(requireContext()).canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS) {
+            val secretKeyName = getString(R.string.secret_key_name)
+            val cipher = cryptographyManager.getInitializedCipherForEncryption(secretKeyName)
+            val biometricPrompt =
+                BiometricPromptUtils.createBiometricPrompt(this, ::encryptAndStoreServerToken)
+            val promptInfo = BiometricPromptUtils.createPromptInfo()
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        }
+    }
+
+    private fun encryptAndStoreServerToken(authResult: BiometricPrompt.AuthenticationResult) {
+        authResult.cryptoObject?.cipher?.apply {
+            SessionManager.getInstance(requireContext()).fetchAuthToken().let { token ->
+                val encryptedServerTokenWrapper = cryptographyManager.encryptData(token!!, this)
+                cryptographyManager.persistCiphertextWrapperToSharedPrefs(
+                    encryptedServerTokenWrapper,
+                    requireContext(),
+                    BIOMETRICS_SHARED_PREFS_FILENAME,
+                    Context.MODE_PRIVATE,
+                    CIPHERTEXT_WRAPPER_KEY
+                )
+            }
+        }
+        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+    }
+
+    private fun showBiometricPromptForDecryption() {
+        ciphertextWrapper?.let { textWrapper ->
+            val secretKeyName = getString(R.string.secret_key_name)
+            val cipher = cryptographyManager.getInitializedCipherForDecryption(
+                secretKeyName, textWrapper.initializationVector
+            )
+            biometricPrompt =
+                BiometricPromptUtils.createBiometricPrompt(
+                    this,
+                    ::decryptServerTokenFromStorage
+                )
+            val promptInfo = BiometricPromptUtils.createPromptInfo()
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        }
+    }
+
+    private fun decryptServerTokenFromStorage(authResult: BiometricPrompt.AuthenticationResult) {
+        ciphertextWrapper?.let { textWrapper ->
+            authResult.cryptoObject?.cipher?.let {
+                val plaintextToken =
+                    cryptographyManager.decryptData(textWrapper.ciphertext, it)
+                SessionManager.getInstance(requireContext()).saveAuthToken(plaintextToken)
+            }
+        }
+        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment LoginFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = LoginFragment()
     }
 }
