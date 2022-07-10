aaa9f07b1517e1e195491a8c0b3ac0f724d01a75
Nicolas Coen <nicocoen@frba.utn.edu.ar>
2022-07-10 19:33:36 -0300

    arreglo lo que rompio eze

--- mobile-app/app/src/main/java/com/mobile/test/api/RetrofitClient.kt
+++ mobile-app/app/src/main/java/com/mobile/test/api/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitClient {
-    private const val BASE_URL = "http:/192.168.0.170:8080/"
+    private const val BASE_URL = "http:/10.0.2.2:8080/"
 //    private const val BASE_URL = "http:/172.17.0.3:8080/"
 
     val service = Retrofit.Builder()
